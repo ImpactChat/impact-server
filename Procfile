42d1ea3eb882da7ccc1f2237be6a7b48967eb6bf
tommcn <tommcn@mcnamer.ca>
2020-12-21 12:48:11 -0500

    Daphne as webserver

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd impact && daphne impact.asgi:application
--- impact/impact/asgi.py
+++ impact/impact/asgi.py
@@ -1,18 +1,25 @@
 import os
 
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
+
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "impact.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 import impactchat.routing
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'impact.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    # Django's ASGI application to handle traditional HTTP requests
+    "http":
+    django_asgi_app,
+
+    # WebSocket chat handler
     "websocket":
-        URLRouter(
-            impactchat.routing.websocket_urlpatterns
-        )
+    URLRouter(impactchat.routing.websocket_urlpatterns)
 })
