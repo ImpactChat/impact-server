7af295ae24f5c35d707a7dd979b4eb216522b528
tommcn <tommcn@mcnamer.ca>
2020-12-28 12:44:31 -0500

    json

--- impact/known/views.py
+++ impact/known/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 def hi(request):
-    return HttpResponse("""{
+    return JsonResponse({
         "associatedApplications": [
             {
             "applicationId": "257171a6-e380-4fb5-8722-9e465f856c29"
             }
         ]
-        }""")
+        })
